c1eb8a98790c5ac786946b88f38b6a943d23981a
lewislea <wlewislea@gmail.com>
2013-06-11 23:22:02 -0500

    new rails. skip test unit. use postgresql.  install devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VehicleMaintenanceSite::Application.config.secret_token = 'd744b84c1ac2fd5ca36cb392bb3b09de2be1d6009a86cabae6e63bc42c34ec1a67399a1f5fbf4828f6b01d6b2a8b6587ee72f7f5f75ca36df8407d657b8daedf'
