23fb9783a7be888ea39d5734b3041d86f058433e
lewislea <wlewislea@gmail.com>
2013-06-26 15:48:09 -0500

    deleted files

--- features/step_definitions/add_vehicle_steps.rb
+++
@@ -1,5 +0,0 @@
-When 'I fill out the date fields' do
-  select '2010', from: 'vehicle_purchased_on_1i'
-  select 'January', from: 'vehicle_purchased_on_2i'
-  select '20', from: 'vehicle_purchased_on_3i'
-end
