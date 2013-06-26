08b40b368a4de701857b7a8b0674eef06fc92f17
lewislea <wlewislea@gmail.com>
2013-06-26 15:14:48 -0500

    repairs deleted with vehicle, records on page ordered by date of creation

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,8 +1,10 @@
 class Vehicle < ActiveRecord::Base
   attr_accessible :vehicle
   attr_accessible :make, :model, :year, :mileage, :purchased_on
-  has_many :repairs
+  has_many :repairs, :dependent => :destroy
   belongs_to :user
+
+    default_scope order("created_at ASC")
 
   validates :year, :mileage, :numericality => true
   validates :make, :model, :year, :mileage, :purchased_on, :presence => true
