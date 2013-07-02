af4f3014f227d0b1f5f3b9bcde97d9accdec155a
lewislea <wlewislea@gmail.com>
2013-07-02 10:23:57 -0500

    ENV secret token added to heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-VehicleMaintenanceSite::Application.config.secret_token = 'd744b84c1ac2fd5ca36cb392bb3b09de2be1d6009a86cabae6e63bc42c34ec1a67399a1f5fbf4828f6b01d6b2a8b6587ee72f7f5f75ca36df8407d657b8daedf'
+if Rails.env.development? or Rails.env.test?
+  VehicleMaintenanceSite::Application.config.secret_token = 'd744b84c1ac2fd5ca36cb392bb3b09de2be1d6009a86cabae6e63bc42c34ec1a67399a1f5fbf4828f6b01d6b2a8b6587ee72f7f5f75ca36df8407d657b8daedf'
+else
+  VehicleMaintenanceSite::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
+
+# heroku config:set SECRET_TOKEN=";alk;icu93jalksdhv;oasgwahtever rake secret"
